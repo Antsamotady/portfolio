864ecca919b2bbe4730c057d59b13129f7dbf816
Antsamotady <hasintsilavina@gmail.com>
2022-09-15 17:19:54 +0300

    add bootstrap in node_modules

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
         <div className="d-flex flex-column flex-root">
           <div className="d-flex flex-column flex-center flex-column-fluid p-10">
             <img src={ errora } className="mw-100 mb-10 h-lg-450px" alt="sary be" />
-            <h1 className="fw-bold mb-10">Seems there is nothing here</h1>
-            <a href="#" className="btn btn-primary">Return Home</a>
+            <h1 className="fw-bold mb-10">Seems like it's working</h1>
+            <p>but not fine yet thoush</p>
+            <a href="#" className="btn btn-primary">Get it work!</a>
           </div>
         </div>
       </div>
