015ec02e33db7c2a3a68010679174d1b17b0e558
Antsamotady <hasintsilavina@gmail.com>
2022-09-15 16:58:40 +0300

    add bootstrap in node_modules

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import './App.css';
 import 'bootstrap/plugins/global/plugins.bundle.css';
 import 'bootstrap/css/style.bundle.css';
 import bigSary from 'bootstrap/media/illustrations/sketchy-1/1-dark.png';
+import errora from 'bootstrap/media/illustrations/sketchy-1/18.png';
 
 function App() {
   return (
     <div className="App">
-
-      <div class="d-flex flex-column flex-root">
-          <div class="d-flex flex-column flex-center flex-column-fluid p-10">
-
-          <img src={ bigSary } className="mw-100 mb-10 h-lg-450px" alt="sary be" />
-          <h1 className="fw-bold mb-10" style="color:#A3A3C7">Seems there is nothing here</h1>
-          <a href="#" className="btn btn-primary">Return Home</a>
+      
+      <div className="auth-bg">
+        <div className="d-flex flex-column flex-root">
+          <div className="d-flex flex-column flex-center flex-column-fluid p-10">
+            <img src={ errora } className="mw-100 mb-10 h-lg-450px" alt="sary be" />
+            <h1 className="fw-bold mb-10">Seems there is nothing here</h1>
+            <a href="#" className="btn btn-primary">Return Home</a>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
