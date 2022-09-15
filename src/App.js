19ce1c324c736b86dfa3d0894b3ae78382e47940
Antsamotady <hasintsilavina@gmail.com>
2022-09-15 16:49:44 +0300

    add bootstrap in node_modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/plugins/global/plugins.bundle.css';
+import 'bootstrap/css/style.bundle.css';
+import bigSary from 'bootstrap/media/illustrations/sketchy-1/1-dark.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div class="d-flex flex-column flex-root">
+          <div class="d-flex flex-column flex-center flex-column-fluid p-10">
+
+          <img src={ bigSary } className="mw-100 mb-10 h-lg-450px" alt="sary be" />
+          <h1 className="fw-bold mb-10" style="color:#A3A3C7">Seems there is nothing here</h1>
+          <a href="#" className="btn btn-primary">Return Home</a>
+        </div>
+      </div>
     </div>
   );
 }
