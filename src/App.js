721d1c0d423cc74137c53aee3624b69b6bc5a580
Antsamotady <hasintsilavina@gmail.com>
2022-10-27 19:31:55 +0300

    Todo app html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import TodoList from './Component/TodoList';
 
 class App extends Component {
 
 render() {
     return (
-      <div>Hi there!</div>
+      <div className='main'>
+        <TodoList />
+        <div className="input-and-button">
+          <input className='form-control' type="text" />
+          <button type='button' className="btn btn-primary">++</button>
+          <button type='button' className="btn btn-secondary">Clear complete</button>
+        </div>
+      </div>
     );
   }
 }
