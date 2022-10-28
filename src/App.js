a4ef1f87ab5802e6df93e51f2d1f76272169ff7b
Antsamotady <hasintsilavina@gmail.com>
2022-10-28 15:36:07 +0300

    Using functionnal component for useEffect

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import TodoList from './Component/TodoList';
 import {v4 as uuidv4} from 'uuid';
+import { useRef } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.todoNameRef = React.createRef();
-  }
+function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
-  state = {
-    todos: []
-  }
-
-  handleAddTodo = event => {
-    const name = this.todoNameRef.current.value;
+  function handleAddTodo() {
+    const name = todoNameRef.current.value;
     if(name === '') return
 
-    const todos = [...this.state.todos];
-    todos.push({id:uuidv4(), name:name, complete:false});
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}];
+    })
+    todoNameRef.current.value = null;
+  } 
 
-    this.setState({todos});
-
-    this.todoNameRef.current.value = null;
-  }
-
-  render() {
-    return (
-      <div>
-        <p className='fs-1 text-center'>TODO</p>
-        <div className='main'>
-          <TodoList todos={this.state.todos}/>
-          
-          <div className="mb-2 d-flex input-and-button">
-            <input className='form-control' type="text" ref={this.todoNameRef}/>
-            <button type='button' className="btn btn-primary" onClick={this.handleAddTodo}>++</button>
-            <button type='button' className="btn btn-secondary">Clear complete</button>
-          </div>
-          <p className='text-center remain'>Remaining todos: <span className='badge'>0</span></p>
+  return (
+    <div>
+      <p className='fs-1 text-center'>TODO</p>
+      <div className='main'>
+        <TodoList todos={todos}/>
+        
+        <div className="mb-2 d-flex input-and-button">
+          <input className='form-control' type="text" ref={todoNameRef}/>
+          <button type='button' className="btn btn-primary" onClick={handleAddTodo}>++</button>
+          <button type='button' className="btn btn-secondary">Clear complete</button>
         </div>
+        <p className='text-center remain'>Remaining todos: <span className='badge'>0</span></p>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default App;
