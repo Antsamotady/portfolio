b59d46b0eba62c4a631895378bcb04ece02e6e02
Antsamotady <hasintsilavina@gmail.com>
2022-11-04 15:21:58 +0300

    Implement images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import React, { useState } from 'react';
+import $ from 'jquery';
+
+import Ly from './public/Ld.JPG';
+import Neh_9_20 from './public/Neh_9_20.jpg';
+import myself from './public/IMG_20200418_172346.jpg';
+import Sal_122 from './public/Sal_122.7.png';
+import saka from './public/IMG_20170514_170825.jpg';
+import Mn from './public/Mn.png';
+import Ephes_6 from './public/Ephes_6.12.png';
+import Dm from './public/Dm.png';
+import swim from './public/vlcsnap-2021-05-08-07h09m42s163.png';
+import Bd from './public/Bd.png';
+import Lio_15_18a from './public/Lio_15_18a.png';
+import kiki from './public/DSC_0344.JPG';
+import II_Tim_4 from './public/II_Tim_4.5.png';
+import cover from './public/cover.png';
+import Dan_12_3 from './public/Dan_12_3.jpg';
+import postal_card from './public/DSC_0273.JPG';
 
 function App() {
   const [color1, setColor1] = useState('#fff');
@@ -10,11 +28,11 @@ function App() {
     background: 'radial-gradient(circle at 100% 100%, #fff, #fff)'
   });
   const [msg, setMsg] = useState({
+    id: 0,
     head: 'Dear visitor',
     body: 'Experience design',
     foot: 'Recently I’ve been interested in web design so I started learning Reactjs. It would be really interesting for me if I make a job of it. I feel the potential it has.'
   })
-
   const onMyMouseMove = e => {
     setPosition({
       posX: Math.round(e.screenX/window.innerWidth * 100),
@@ -28,34 +46,39 @@ function App() {
   
   const handleButtonclick = e => {
     let color2 = '#fff';
-    let msgHead; let msgBody; let msgFoot;
+    let msgId; let msgHead; let msgBody; let msgFoot;
     msgHead = msgBody = msgFoot = '';
     if(e.target.value === 'a0') {
       color2 = '#ff0000';
+      msgId = 1;
       msgHead = 'Hello! I am';
       msgBody = 'Garry';
       msgFoot = 'I am a web developer actually oriented to graphic design. Freelance web designer, please send me an email if you are interested in my work.';
     }
     if(e.target.value === 'a') {
       color2 = 'rgb(36, 69, 36)';
+      msgId = 2;
       msgHead = 'Know me more through';
       msgBody = 'Photography';
-      msgFoot = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatibus nostrum consectetur perferendis vel iure incidunt accusantium molestias a corporis aspernatur facilis voluptas similique, cumque nam dignissimos explicabo omnis voluptates!';
+      msgFoot = 'Here are some of my works';
     }
     if(e.target.value === 'b') {
       color2 = '#0000ff';
+      msgId = 3;
       msgHead = 'Have a look at my';
       msgBody = 'Digital Painting';
       msgFoot = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatibus nostrum consectetur perferendis vel iure incidunt accusantium molestias a corporis aspernatur facilis voluptas similique, cumque nam dignissimos explicabo omnis voluptates!';
     }
     if(e.target.value === 'c') {
       color2 = '#1e1e1e';
+      msgId = 4;
       msgHead = 'Look at';
       msgBody = 'Text art';
       msgFoot = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatibus nostrum consectetur perferendis vel iure incidunt accusantium molestias a corporis aspernatur facilis voluptas similique, cumque nam dignissimos explicabo omnis voluptates!';
     }
     if(e.target.value === 'd') {
       color2 = '#fff';
+      msgId = 5;
       msgHead = 'Simple';
       msgBody = 'Visual design';
       msgFoot = 'From here I invite you to have a look at my website experiment';
@@ -67,7 +90,7 @@ function App() {
 
     setColor1(color2);
 
-    setMsg({head:msgHead, body:msgBody, foot:msgFoot })
+    setMsg({id:msgId, head:msgHead, body:msgBody, foot:msgFoot })
 
   }
 
@@ -87,6 +110,34 @@ function App() {
           <div className="txt txt-head">{msg.head}</div>
           <div className="txt txt-body">{msg.body}</div>
           <div className="txt txt-foot">{msg.foot}</div>
+
+          <div className="container" style={{ display: msg.id===2 ? 'block' : 'none' }}>
+
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src={Ly} className="d-block w-100" alt="aaa" />
+                </div>
+                <div className="carousel-item">
+                  <img src={swim} className="d-block w-100" alt="bbb" />
+                </div>
+                <div className="carousel-item">
+                  <img src="..." className="d-block w-100" alt="ccc" />
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+
+          </div>
+
+
         </div>
         
         <footer className='tsl-footer'>
@@ -123,7 +174,7 @@ function App() {
             </div>
           </div>
         </footer>
-      </div>  
+      </div>
     </div>
   );
 
